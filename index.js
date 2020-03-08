8f4bdc2254c50fe7fe96b9dda37eff780ff2c333
Markus Hunsalz <markushunsalz@Markuss-MBP.fritz.box>
2020-03-08 18:26:41 +0100

    added error logging for fetching Wikidata

--- index.js
+++ index.js
@@ -50,9 +50,8 @@ class WikidataSource {
 
     api.loadSource(async actions => {
       // fetch data ...
-      this.info("Fetching Wikidata ...");
       const downloads = await this.fetchWikidata(actions);
-      // download remote URIs
+      // download remote URIs ...
       if (process.env.DOWNLOAD_MEDIA === "true") {
         this.info("Starting media download(s) ...");
         await this.download(downloads);
@@ -71,39 +70,45 @@ class WikidataSource {
   }
 
   async fetchWikidata(actions) {
+    this.info("Fetching Wikidata ...");
     const queryDispatcher = new SPARQLQueryDispatcher(this.options.url);
     const collection = actions.addCollection({ typeName: "Record" });
     const downloads = [];
     const dir = process.cwd() + this.options.baseDir;
     // query Wikidata and process items
-    await queryDispatcher.query(this.options.sparql).then(response => {
-      // parse JSON body
-      let body = JSON.parse(response.body);
-      // process each item
-      body.results.bindings.forEach(item => {
-        // inspect & rewrite item properties
-        Object.keys(item).forEach(property => {
-          // rewrite URI with the later download file reference
-          if (item[property].type === "uri") {
-            let uri = item[property].value;
-            let filename = uri.substring(uri.lastIndexOf("/") + 1);
-            filename = decodeURI(filename).replace(/%2C/g, ",");
-            // TODO file name
-            downloads.push({
-              src: uri,
-              dir: dir,
-              filename: filename
-            });
-            // rewrite value with absolute local file path
-            item[property].value = dir + filename;
-          }
-          // flatten objects by extracting values only
-          item[property] = item[property].value;
+    await queryDispatcher
+      .query(this.options.sparql)
+      .then(response => {
+        // parse JSON body
+        let body = JSON.parse(response.body);
+        // process each item
+        body.results.bindings.forEach(item => {
+          // inspect & rewrite item properties
+          Object.keys(item).forEach(property => {
+            // rewrite URI with the later download file reference
+            if (item[property].type === "uri") {
+              let uri = item[property].value;
+              let filename = uri.substring(uri.lastIndexOf("/") + 1);
+              filename = decodeURI(filename).replace(/%2C/g, ",");
+              // TODO file name
+              downloads.push({
+                src: uri,
+                dir: dir,
+                filename: filename
+              });
+              // rewrite value with absolute local file path
+              item[property].value = dir + filename;
+            }
+            // flatten objects by extracting values only
+            item[property] = item[property].value;
+          });
+          // add collection
+          collection.addNode(item);
         });
-        // add collection
-        collection.addNode(item);
+      })
+      .catch(error => {
+        console.log("Fetching Wikidata failed!", error);
       });
-    });
     return downloads;
   }
 
